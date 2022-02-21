038951535c093c62d0e5b16881cd349fbc14dfc3
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-02-22 01:11:06 +0900

    feat : Create userRepository

---
+++ src/main/java/com/saveme/comparator/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.saveme.comparator.repository;
+
+import com.saveme.comparator.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
