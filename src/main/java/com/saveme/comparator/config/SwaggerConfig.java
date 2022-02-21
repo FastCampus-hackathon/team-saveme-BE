8a23426a1a2434874c9000abfb7e24cfb5579b53
Jeongisu94 <mustdoit2020@gmail.com>
2022-02-21 16:56:03 +0900

    feat : swagger 3.0 적용

---
+++ src/main/java/com/saveme/comparator/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.saveme.comparator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder()
+                .title("API")
+                .description("API DOCs").build();
+    }
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.saveme.comparator.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(swaggerInfo());
+    }
+}
