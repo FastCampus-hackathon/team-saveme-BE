eda88cef4b74f533485b1eb9e1fddb6fd437b3e8
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-02-23 03:53:07 +0900

    bugfix : add @Param

--- src/main/java/com/saveme/comparator/repository/WishRepository.java
+++ src/main/java/com/saveme/comparator/repository/WishRepository.java
@@ -4,6 +4,7 @@ import com.saveme.comparator.domain.User;
 import com.saveme.comparator.domain.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,6 +14,6 @@ public interface WishRepository extends JpaRepository<Wish, Long> {
 
     void deleteByUserAndRecruition_RecruitmentId(User user, Long recruitmentId);
 
-    @Query("select w from Wish w join fetch w.recruition where w.user =: user")
-    List<Wish> findAllByUser(User user);
+    @Query("select w from Wish w join fetch w.recruition where w.user.userId=:userId")
+    List<Wish> findAllByUser_UserId(@Param("userId")Long userId);
 }
