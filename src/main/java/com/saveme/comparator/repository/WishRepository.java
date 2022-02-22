cb7b4a100ce20d753358bdc3654053c61c624b5b
SENUNGHOONYANG <sheepsh93@gmail.com>
2022-02-23 01:44:39 +0900

    feat : Create findALlByUser method into WishRepository

--- src/main/java/com/saveme/comparator/repository/WishRepository.java
+++ src/main/java/com/saveme/comparator/repository/WishRepository.java
@@ -1,12 +1,18 @@
 package com.saveme.comparator.repository;
 
-import com.saveme.comparator.domain.Recruition;
 import com.saveme.comparator.domain.User;
 import com.saveme.comparator.domain.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface WishRepository extends JpaRepository<Wish,Long> {
+import java.util.List;
+
+public interface WishRepository extends JpaRepository<Wish, Long> {
 
     boolean existsByUserAndRecruition_RecruitmentId(User user, Long recruitmentId);
+
     void deleteByUserAndRecruition_RecruitmentId(User user, Long recruitmentId);
+
+    @Query("select w from Wish w join fetch w.recruition where w.user =: user")
+    List<Wish> findAllByUser(User user);
 }
